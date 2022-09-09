09084401595414afc7ec596112bece655ac2e845
jeongwon <choigarden@nate.com>
2022-09-10 00:00:25 +0900

    14-3 액션 함수 추가 하기

--- MoviePlayer/ViewController.swift
+++ MoviePlayer/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
